50eef4415e8e83d39233b8c39238acdc716094b7
moutosi-raka <raka29077997@gmail.com>
2022-11-08 17:11:07 +0600

    added home services

---
+++ src/pages/Home/Services/Services.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import ServicesCart from "./ServicesCart";
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+  
+    useEffect(() => {
+      fetch("services.json")
+        .then((res) => res.json())
+        .then((data) => setServices(data));
+    }, []);
+    const newArrayServices = services.slice(0, 3);
+    return (
+        <div className="my-16 p-20  bg-pink-50">
+      <div className="text-center mb-8">
+        <h1 className="text-8xl font-bold dancing">Our Services</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
+          incidunt placeat pariatur odio cumque.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {newArrayServices.map((service) => (
+          <ServicesCart
+            key={service.service_id}
+            service={service}
+          ></ServicesCart>
+        ))}
+      </div>
+      <div className="text-center mt-8">
+        <Link to='/services'><button className="btn btn-outline btn-secondary">See All Services</button></Link>
+      </div>
+    </div>
+    );
+};
+
+export default Services;
---
+++ src/pages/Home/Services/ServicesCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesCart = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ServicesCart;
