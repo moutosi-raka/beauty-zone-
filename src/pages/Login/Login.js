8614e1eb140e7bf349ce2a398abf8197ec418a09
moutosi-raka <raka29077997@gmail.com>
2022-11-08 17:55:25 +0600

    added login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,69 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import Header from "../Shared/Header/Header";
 
 const Login = () => {
+
+    const handleLogin = event =>{
+
+    }
     return (
         <div>
-            login
+        <Header></Header>
+      <div className="w-full max-w-md p-8 space-y-3 rounded-xl text-white mx-auto my-20 pink-gradient shadow-lg">
+        <h1 className="text-2xl font-bold text-center ">Login</h1>
+        <form onSubmit={handleLogin} className="space-y-6 ng-untouched ng-pristine ng-valid">
+          <div className="space-y-1 text-sm">
+            <label htmlFor="username" className="block  ">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="w-full px-4 py-3 text-black rounded-md"
+            />
+          </div>
+          <div className="space-y-1 text-sm">
+            <label htmlFor="password" className="block \">
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              className="w-full px-4 py-3 rounded-md border-gray-700 text-black focus:border-violet-400"
+            />
+            <div className="flex justify-end text-xs text-white">
+            </div>
+          </div>
+          <button   className="block w-full p-3 text-center rounded-sm  bg-pink-500">
+            Log in
+          </button>
+        </form >
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 bg-base-100"></div>
+          <p className="px-3 text-sm ">
+            Login with social accounts
+          </p>
+          <div className="flex-1 h-px sm:w-16 bg-base-100"></div>
         </div>
+        <div className="flex justify-center space-x-4">
+          
+        </div>
+        <p className="text-sm text-center sm:px-6 ">
+          Don't have an account?
+          <Link
+            rel="noopener noreferrer"
+            to="/signup"
+            className="underline text-pink-900 font-bold ml-2"
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </div>
     );
 };
 
