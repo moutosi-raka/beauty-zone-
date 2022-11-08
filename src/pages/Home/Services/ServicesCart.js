4846aef5985f52fe67745001d06308476a63071c
moutosi-raka <raka29077997@gmail.com>
2022-11-08 17:19:10 +0600

    added home page services cart

--- src/pages/Home/Services/ServicesCart.js
+++ src/pages/Home/Services/ServicesCart.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-const ServicesCart = () => {
+const ServicesCart = ({service}) => {
+    const {  service_name, price, details, img } = service;
     return (
-        <div>
-            
+        <div className='px-8'>
+        <div className='text-center px-4 ml-4'>
+           <img className='rounded-full w-64 h-64 border-8 border-white' src={img} alt="" />
         </div>
+        <div className='text-center'>
+        <h2 className="text-4xl font-bold dancing">{service_name}</h2>
+        <p>{details.slice(0,100) + "..."}</p>
+        <p className='font-bold'>Price : {price}</p>
+        </div>
+    </div>
     );
 };
 
