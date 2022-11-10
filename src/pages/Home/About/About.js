ccdb5fe0026f5417772ada6a083fca9273e38f2e
moutosi-raka <raka29077997@gmail.com>
2022-11-10 16:58:16 +0600

    remove about button

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
           <p className="py-6">
           Beauty Zone Spa in Dhaka. We neglect hands and feet the most in beauty. The skin of the hand becomes black and rough due to daily dust and sun. Manicure should be done regularly to maintain the radiance of the skin of the hands. You can easily do manicure at home. You don’t have to take the time to go to the parlor. You can easily do manicure sitting at home. Let’s not know how to do a manicure at home. reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.
           </p>
-          <button className="btn bg-pink-400 w-40">Read More</button>
+          
         </div>
       </div>
     </div>
